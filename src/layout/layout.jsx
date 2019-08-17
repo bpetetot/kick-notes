5d3387ac90ec0f1259c5a7585a11963144fe4a36
Benjamin Petetot <bpetetot@gmail.com>
2019-08-17 16:40:11 +0200

    :lipstick: use CSS variables

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import cn from 'classnames'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import { useAuth, AUTH_REDIRECT_PATH } from 'services/auth'
 import { SiderProvider } from 'components/Sider'
 import Loading from 'components/Loading'
 import PrivateRoute from 'services/auth/PrivateRoute'
-import theme from 'styles/theme.module.css'
 
 import Header from './Header'
 import styles from './layout.module.css'
@@ -18,7 +16,7 @@ const Layout = () => {
 
   return (
     <BrowserRouter>
-      <div className={cn(styles.layout, theme.default)}>
+      <div className={styles.layout}>
         <SiderProvider>
           <Header />
           <div className={styles.main}>
