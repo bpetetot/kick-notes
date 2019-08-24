22e825b887d7948679a6a0d91e3401b51c9726c0
Benjamin Petetot <bpetetot@gmail.com>
2019-08-24 19:24:01 +0200

    :lipstick: better style on mobile

--- src/services/navigation/context.js
+++ src/services/navigation/context.js
@@ -44,8 +44,6 @@ const NavigationProvider = ({ children, location, history }) => {
     }
   }
 
-  console.log({ path, res: revealDots(path) })
-
   return (
     <NavigationContext.Provider
       value={{
